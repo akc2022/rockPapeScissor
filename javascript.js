c8ce95997ee7e130a33666b3648991e01b53291f
Alli C <akcallah@gmail.com>
2022-09-14 10:22:47 -0700

    add function to get user input

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@ function getComputerChoice () {
 let arrayOfChoices = ['Rock','Paper','Scissors'];
 return arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
 }
+
+function printUserInput () {
+  const playerSelection = prompt("Please select your weapon");
+  return playerSelection;
+}
