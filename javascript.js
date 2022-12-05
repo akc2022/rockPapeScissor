45276461d003e4185d808365862a432b2ef3c4a8
Alli C <akcallah@gmail.com>
2022-12-05 10:44:24 -0800

    add buttons and display results

--- javascript.js
+++ javascript.js
@@ -1,58 +1,71 @@
 
 let userScore=0;
 let computerScore=0;
+let buttons=document.querySelectorAll('button');
+let results =document.getElementById('results');
+let computerChoiceDisplay = document.getElementById('computerChoice')
+let score = document.getElementById('displayScore')
+score.textContent = "0 - 0";
+let body = document.body
+
+for (i=0; i<buttons.length; i++) {
+  buttons[i].addEventListener('click', playRound);
+}
+
+// buttons[0].addEventListener('click', playRound);
+
+
+
 
 function getComputerChoice () {
 let arrayOfChoices = ['rock','paper','scissors'];
 return arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
 }
 
-function getUserChoice() {
-let validResponse = false;
-const validEntries = new Set (["rock", "paper", "scissors"]);
-while (!validResponse) {
-let playerSelection = prompt("Please select your weapon! rock, paper or scissors");
-playerSelection=playerSelection.toLowerCase();
-if (validEntries.has(playerSelection)) {
-  validResponse=true;
-  return playerSelection;
-}
-}
-}
-
-function playRound (){
-  const computerSelection = getComputerChoice();
-  let playerSelection=getUserChoice();
-  console.log(playerSelection);
-  console.log(computerSelection);
-  if (computerSelection=='paper' && playerSelection=='rock' || computerSelection=='scissors' && playerSelection=="paper" || computerSelection=='rock' && playerSelection=="scissors") {
-     console.log( `You LOSE this round! ${computerSelection} beats ${playerSelection}  `);
-    return computerScore+=1;
+function playRound (e){
+  if (userScore == 5  || computerScore == 5) {
+    return;
+  }
+  else {
+const playerSelection = e.target.id;
+const computerSelection = getComputerChoice()
+computerChoiceDisplay.textContent = computerSelection
+if (computerSelection=='paper' && playerSelection=='rock' || computerSelection=='scissors' && playerSelection=="paper" || computerSelection=='rock' && playerSelection=="scissors") {
+     results.textContent= `You LOSE this round! ${computerSelection} beats ${playerSelection}  `;
+    computerScore+=1
+    if (computerScore == 5) {
+      body.innerHTML="<h1>You lose! The computer is victorious once again!</h1>"
+    };
   }
   else if (playerSelection=='paper' && computerSelection=="rock" || playerSelection=='scissors' && computerSelection=="paper" || playerSelection=='rock' && computerSelection=="scissors") {
-      console.log( `You Win this round! ${playerSelection} beats ${computerSelection}  `);
-      return userScore+=1;
+      results.textContent =  `You WIN this round! ${playerSelection} beats ${computerSelection}  `;
+      userScore+=1
+      if (userScore== 5) {
+        body.innerHTML="<h1>You win!</h1>"
+      };
     }
   else {
-    console.log("Tie!");
+    results.textContent = "Tie!" ;
   }
+score.textContent = `${userScore} - ${computerScore}`
+}
 }
 
 
-function game() {
-  for (i=1; i<=5; i++) {
-  playRound();
-  }
-  if (userScore > computerScore) {
-    console.log("Congrats! You beat the computer");
-  }
-  else if (computerScore > userScore) {
-    console.log("Womp womp womp! YOU LOSE!");
-  }
-  else {
-    console.log("You tied with the computer!");
-  }
+// function game() {
+//   for (i=1; i<=5; i++) {
+//   playRound();
+//   }
+//   if (userScore > computerScore) {
+//     console.log("Congrats! You beat the computer");
+//   }
+//   else if (computerScore > userScore) {
+//     console.log("Womp womp womp! YOU LOSE!");
+//   }
+//   else {
+//     console.log("You tied with the computer!");
+//   }
+//
+// }
 
-}
-
-game ();
+// game ();
